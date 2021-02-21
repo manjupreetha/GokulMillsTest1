c00bad7f71f6240b91e010ebb6a238a93aa33fcb
manjupreetha <45552813+manjupreetha@users.noreply.github.com>
2021-02-21 19:19:44 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'Products',
+    loadChildren : () => import('./products/products.module').then( m => (m.ProductsModule))
+  },
+  {
+    path : '',
+    loadChildren : () => import('./home/home.module').then( m => (m.HomeModule))
+  },
+  {
+    path : 'whyus',
+    loadChildren : () => import('./why-us/why-us.module').then( m => (m.WhyUsModule))
+  },
+  {
+    path : 'contact',
+    loadChildren : () => import('./contact/contact.module').then( m => (m.ContactModule))
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
